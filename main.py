f35e6b34f47e4f434a414286c9d3943324e2d098
J1llKews <magneficient@tutamail.com>
2026-03-31 20:29:35 +0300

    main.py

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 from poker import Texas_holdem, Omaha_holdem
 
-print("выберите режим игры")
-print("1 - texas hold'em")
-print("2 - omaha hold'em")
+def main():
+    print("выберите режим игры")
+    print("1 - texas hold'em")
+    print("2 - omaha hold'em")
 
-choice = input("ваш выбор: ")
+    choice = input("ваш выбор: ")
 
-if choice == "1":
-    print("texas hold'em")
+    if choice == "1":
+        print("\ntexas hold'em")
 
-    a1 = input("введите значение первой карты: ")
-    m1 = input("введите масть первой карты: ")
+        a1 = input("введите значение первой карты: ")
+        m1 = input("введите масть первой карты: ")
 
-    a2 = input("введите значение второй карты: ")
-    m2 = input("введите масть второй карты: ")
+        a2 = input("введите значение второй карты: ")
+        m2 = input("введите масть второй карты: ")
 
-    t = Texas_holdem(a1, m1, a2, m2)
-    t.pocket_info()
-    t.win_chance()
+        t = Texas_holdem(a1, m1, a2, m2)
+        t.pocket_info()
+        t.win_chance()
 
-elif choice == "2":
-    print("omaha hold'em")
+    elif choice == "2":
+        print("\nomaha hold'em")
 
-    a1 = input("введите значение 1й карты: ")
-    m1 = input("введите масть 1й карты: ")
+        a1 = input("введите значение 1й карты: ")
+        m1 = input("введите масть 1й карты: ")
 
-    a2 = input("введите значение 2й карты: ")
-    m2 = input("введите масть 2й карты: ")
+        a2 = input("введите значение 2й карты: ")
+        m2 = input("введите масть 2й карты: ")
 
-    a3 = input("введите значение 3й карты: ")
-    m3 = input("введите масть 3й карты: ")
+        a3 = input("введите значение 3й карты: ")
+        m3 = input("введите масть 3й карты: ")
 
-    a4 = input("введите значение 4й карты: ")
-    m4 = input("введите масть 4й карты: ")
+        a4 = input("введите значение 4й карты: ")
+        m4 = input("введите масть 4й карты: ")
 
-    o = Omaha_holdem(a1, m1, a2, m2, a3, m3, a4, m4)
-    o.pocket_info()
-    o.win_chance()
+        o = Omaha_holdem(a1, m1, a2, m2, a3, m3, a4, m4)
+        o.pocket_info()
+        o.win_chance()
 
-else:
-    print("неверно")
+    else:
+        print("неверно")
+
+
+main()
